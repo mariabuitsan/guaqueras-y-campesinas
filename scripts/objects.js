01f67cf707d86acf9e605256fe0065ce8a7360b6
mariabuitsan <mbuitrago@gradcenter.edu.co>
2024-12-24 11:29:01 -0500

    updates-objects-page

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,7 +1,23 @@
-//load DOM
+//import arrays
+import { objectsCollection } from './objectsArray.js';
 document.addEventListener('DOMContentLoaded', function () {
-  let dialogBox = document.querySelector('.dialogue-box')
-    document.querySelector('.clickable-region').addEventListener('click', () => {
-        dialogBox.style.display = 'block';
-      });
+ 
+  const imageContainer = document.querySelector('.image-objeto-slider');
+  let currentImageIndex = 0;
+  //style for transition
+  imageContainer.style.transition = 'all 0.2s ease-in-out';
+
+  //event listener for mouse over
+  imageContainer.addEventListener('click', () => {
+    imageContainer.style.backgroundImage = `url("${objectsCollection[0]}")`;
+  });
+  //event listener for mouse out
+  imageContainer.addEventListener('mouseout',  () => {
+  imageContainer.style.boxShadow = 'none';
+    
+    //cycle through images 
+    currentImageIndex = (currentImageIndex + 1) % objectsCollection.length;
+    imageContainer.style.backgroundImage = `url("${objectsCollection[currentImageIndex]}")`;
 });
+});
+
---
+++ scripts/objectsArray.js
@@ -0,0 +1,5 @@
+export var objectsCollection = [
+    'assets/img/agricultora-cafe-1.png',
+    'assets/img/agricultora-caña-objeto-1.png',
+    'assets/img/background-4-cecropia-peltata-blanco-y-negro.JPG',
+]
