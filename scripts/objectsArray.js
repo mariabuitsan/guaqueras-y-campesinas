7dc882895fb9097e65bd2ad29cc4e2dcfcf00b1c
mariabuitsan <mbuitrago@gradcenter.edu.co>
2024-12-24 12:17:10 -0500

    added-photos

--- scripts/objectsArray.js
+++ scripts/objectsArray.js
@@ -1,5 +1,10 @@
 export var objectsCollection = [
-    'assets/img/agricultora-cafe-1.png',
-    'assets/img/agricultora-caña-objeto-1.png',
-    'assets/img/background-4-cecropia-peltata-blanco-y-negro.JPG',
+    'assets/img/CV-foto-2.JPG',
+    'assets/img/labores-caña-cachaza-1.JPG',
+    'assets/img/labores-caña-cachaza-2.JPG',
+    'assets/img/objeto-1-caña-cusbira-zoom.JPG',
+    'assets/img/panela-con-quajada.jpeg',
+    'assets/img/agricultora-caña-1.JPG',
+    'assets/img/background-caña-1.JPG',
+    'assets/img/cusbira-colombianismos.png',
 ]
