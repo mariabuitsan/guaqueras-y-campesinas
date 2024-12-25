d982b738eb7df07db16bb029cbd9d793b3f32254
mariabuitsan <mbuitrago@gradcenter.edu.co>
2024-12-25 13:37:46 -0500

    about-page

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+import {objectsCollection} from './objectsArray.js';
 //load DOM
 document.addEventListener('DOMContentLoaded', function () {
     //get the element for images rotation
@@ -28,15 +29,49 @@ document.addEventListener('DOMContentLoaded', function () {
         currentImageIndex = (currentImageIndex + 1) % imageArray.length;
         imageContainer.style.backgroundImage = `url("${imageArray[currentImageIndex]}")`;
     });
-    document.querySelector('.scroller').addEventListener('click', function() {
-         //Scroll down by one viewport height
-        window.scrollBy({
-          top: window.innerHeight,
-         behavior: 'smooth'  // Smooth scroll effect
-        });
-    });
-      
     document.getElementById("transcripcion").addEventListener("click", function() {
         window.location.href = 'entrevistas.html';  
     });
+    
+    document.querySelectorAll('.scroller').forEach(function(element) {
+        element.addEventListener('click', function() {
+            // Scroll down by one viewport height
+            window.scrollBy({
+                top: window.innerHeight,
+                behavior: 'smooth' // Smooth scroll effect
+            });
+        });
+   });
+    let changePhoto = document.getElementById("grid-item-2");
+    currentImageIndex = (currentImageIndex + 0) % objectsCollection.length;
+    changePhoto.style.backgroundImage = `url("${objectsCollection[currentImageIndex]}")`;
+
+    let changePhoto2 = document.getElementById("grid-item-3");
+    currentImageIndex = (currentImageIndex + 1) % objectsCollection.length;
+    changePhoto2.style.backgroundImage = `url("${objectsCollection[currentImageIndex]}")`;
+
+    let changePhoto3 = document.getElementById("grid-item-4");
+    currentImageIndex = (currentImageIndex + 2) % objectsCollection.length;
+    changePhoto3.style.backgroundImage = `url("${objectsCollection[currentImageIndex]}")`;
+
+    let changePhoto4 = document.getElementById("grid-item-5");
+    currentImageIndex = (currentImageIndex + 3) % objectsCollection.length;
+    changePhoto4.style.backgroundImage = `url("${objectsCollection[currentImageIndex]}")`;
+
+    let changePhoto5 = document.getElementById("grid-item-6");
+    currentImageIndex = (currentImageIndex + 4) % objectsCollection.length;
+    changePhoto5.style.backgroundImage = `url("${objectsCollection[currentImageIndex]}")`;
+
+    document.getElementById("show-about").addEventListener('click', function() {
+        //hide things
+        let hidePhoto = document.querySelector('.image-1-narracion');
+        hidePhoto.style.display = "none";
+
+        let hideStory = document.querySelector('.intro');
+        hideStory.style.display = "none";
+        //change div to display
+        let showAbout = document.getElementById("about");
+        showAbout.style.display = "block";
+    });
 });
+ 
